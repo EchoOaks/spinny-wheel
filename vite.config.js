002fa1219fd50ab3162a6075005755b1a579a929
EchoOaks <theyappstore@gmail.com>
2024-07-02 11:57:46 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,16 @@
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-export default {
-    plugins: [
-        vue()
-    ],
-    server: {
-        port: 8000,
-        strictPort: true,
-        watch: {
-          // 3. tell vite to ignore watching `src-tauri`
-          ignored: ["**/src-tauri/**"],
-        },
-      },
-    build: {
-        lib: {
-            entry: 'src/main.js',
-            name: 'VueWheelSpinner',
-            fileName: 'vue-wheel-spinner'
-        },
-        rollupOptions: {
-            external: ['vue'],
-            output: {
-                globals: {
-                    vue: 'Vue'
-                }
-            }
-        }
-    }
-}
+// https://vitejs.dev/config/
+export default defineConfig(async () => ({
+  plugins: [vue()],
+  clearScreen: false,
+  // fixed port expected, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true,
+    watch: {
+      ignored: ["**/src-tauri/**"],
+    },
+  },
+}));
